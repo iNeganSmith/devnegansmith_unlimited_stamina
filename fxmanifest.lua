04ca9616fb40881670bf5d19569e181f3cdef749
RyuKazu <pacheco97j@gmail.com>
2026-09-19 13:12:36 -0500

    docs: ampliar compatibilidad ESX QBCore Qbox y Standalone

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'DevNeganSmith'
-description 'DevNeganSmith - Stamina ilimitada sin modificar la velocidad de carrera'
+description 'Stamina ilimitada sin modificar la velocidad. Standalone, compatible con ESX Legacy, QBCore y Qbox/QBX.'
 version '1.0.0'
 
 client_script 'client.lua'
