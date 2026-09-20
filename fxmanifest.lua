0acc09778fd68e3a9db487509cdf1be353f03c32
RyuKazu <pacheco97j@gmail.com>
2026-09-20 10:53:48 -0500

    chore: prepare v1.0.2 for FiveM asset delivery

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,6 @@ game 'gta5'
 
 author 'DevNeganSmith'
 description 'Stamina ilimitada sin modificar la velocidad. Standalone, compatible con ESX Legacy, QBCore y Qbox/QBX.'
-version '1.0.1'
+version '1.0.2'
 
 client_script 'client.lua'
